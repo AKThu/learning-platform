003d01b86ec0198ccd089543eddb1b7674ae40d1
AKThu <aungkhantthu002@gmail.com>
2024-05-08 23:26:22 +0630

    basic layout design created, navigation bar added, courses layout design created

---
+++ resources/views/components/hero-section.blade.php
@@ -0,0 +1,3 @@
+<div class="min-h-screen flex justify-center items-center text-5xl text-gray-50 font-bold select-none bg-gradient-to-b from-indigo-900 to-gray-800">
+    Hero Section
+</div>
---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,34 @@
+<div class="fixed w-full">
+    <nav class="flex flex-row justify-between px-8 py-4 bg-gray-900 text-gray-50 drop-shadow-md">
+        <a 
+            href="/"
+            class="text-2xl font-medium"
+        >
+            Unnamed Learn
+        </a>
+        <div>
+            @auth
+                <form action="logout" method="POST">
+                    @csrf
+                    <button
+                        type="submit"
+                        class=""
+                    >
+                        Logout
+                    </button>
+                </form>
+            @else
+                <div class="space-x-4">
+                    <a
+                        href="/login"
+                    >
+                        Login
+                    </a>
+                    <a href="/register">
+                        Register
+                    </a>
+                </div>
+            @endauth
+        </div>
+    </nav>
+</div>
